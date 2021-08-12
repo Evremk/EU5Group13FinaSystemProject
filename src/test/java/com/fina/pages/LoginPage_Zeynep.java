0b5d8cba0945d7f786a4e524f8b997595963b65b
Zeynep-bak <zeynepsimsek81@mail.com>
2021-08-12 10:27:19 +0200

    Initial commit

---
+++ src/test/java/com/fina/pages/LoginPage_Zeynep.java
@@ -0,0 +1,17 @@
+package com.fina.pages;
+
+import com.fina.utilities.Driver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class LoginPage_Zeynep {
+
+    public LoginPage_Zeynep(){
+        PageFactory.initElements(Driver.get(), this);
+    }
+
+        // verification that we logged
+    }
+
+
