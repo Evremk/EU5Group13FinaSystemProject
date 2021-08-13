993f6f8f96edd4f8b5c48d280f5e50f7d7df1056
MerveKeles <merve-2007@hotmail.de>
2021-08-13 09:53:15 +0200

    hello

--- src/test/java/com/fina/pages/LoginPage_Merve.java
+++ src/test/java/com/fina/pages/LoginPage_Merve.java
@@ -4,5 +4,7 @@ public class LoginPage_Merve {
 
     public static void main(String[] args) {
         System.out.println("Test");
+        System.out.println("Test2");
+        System.out.println("Test3");
     }
 }
