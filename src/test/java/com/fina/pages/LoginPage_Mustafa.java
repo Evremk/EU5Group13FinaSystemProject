26d8df972cdbda23dd046e29c00064e8c7915937
yhynvrz <navrizyahya@gmail.com>
2021-08-26 19:23:09 +0200

    Merhaba

--- src/test/java/com/fina/pages/LoginPage_Mustafa.java
+++ src/test/java/com/fina/pages/LoginPage_Mustafa.java
@@ -3,5 +3,6 @@ package com.fina.pages;
 public class LoginPage_Mustafa {
     public static void main(String[] args) {
         System.out.println("Merhaba");
+        System.out.println("Hallo");
     }
 }
